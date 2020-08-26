aa4896d4fafdf9c49130ca16c6d7570f85309426
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2020-08-26 11:56:44 +0200

    6

--- objects_4.js
+++ objects_4.js
@@ -1,13 +1,11 @@
 // Returns a random DNA base
-const returnRandBase = () => 
-{
+const returnRandBase = () => {
     const dnaBases = ['A', 'T', 'C', 'G']
     return dnaBases[Math.floor(Math.random() * 4)]
 }
 
 // Returns a random single stand of DNA containing 15 bases
-const mockUpStrand = () => 
-{
+const mockUpStrand = () => {
     const newStrand = []
     for (let i = 0; i < 15; i++) {
         newStrand.push(returnRandBase())
@@ -16,13 +14,22 @@ const mockUpStrand = () =>
 }
 
 const pAequorFactory = (specimenNum, dna) => {
-    const organism =
-    {
+    return {
         specimenNum,
-        dna
-    }
-    return organism;
+        dna,
+        mutate() {
+            for (let i in dna) {
+                let buf = dna[i];
+                //console.log("old: " + dna[i]);
+                do {
+                    dna[i] = returnRandBase();
+                } while (dna[i] === buf);
+                //console.log("new: " + dna[i]);
+            }
+        }
+    };
 }
 
-
+/let org = pAequorFactory(1, mockUpStrand());
+//org.mutate();
 //console.log(pAequorFactory(1, mockUpStrand()));
